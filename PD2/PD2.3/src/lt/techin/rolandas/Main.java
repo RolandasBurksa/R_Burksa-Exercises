fca59eebfc21b091b83f2badc7147c2face0afbe
RolandasBurksa <rburksa@gmail.com>
2023-12-24 00:36:52 +0200

    Moved PD2 Added PD2.3

---
+++ PD2/PD2.3/src/lt/techin/rolandas/Main.java
@@ -0,0 +1,16 @@
+package lt.techin.rolandas;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        System.out.println("a\t   a^2\t\ta^3\t\ta^4");
+        System.out.println("___________________________");
+        System.out.println("1\t   1\t\t1\t\t1");
+        System.out.println("2\t   4\t\t8\t\t16");
+        System.out.println("3\t   9\t\t27\t\t81");
+        System.out.println("4\t   16\t\t64\t\t256");
+
+    }
+
+}
