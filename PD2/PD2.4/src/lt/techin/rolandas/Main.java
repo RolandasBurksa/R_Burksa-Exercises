2d9597df2dc3a32c826aed15691a179ba8c555b7
RolandasBurksa <rburksa@gmail.com>
2023-12-24 01:11:46 +0200

    Moved PD2 Added PD2.3

---
+++ PD2/PD2.4/src/lt/techin/rolandas/Main.java
@@ -0,0 +1,17 @@
+package lt.techin.rolandas;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+                System.out.print("Iveskite savo varda: ");
+
+            String userName = sc.nextLine();
+
+                System.out.print("Sveiki, "+userName +"!");
+        }
+    }
