bf8749f86741e1fd4d985a0ce39417a0698d9638
RolandasBurksa <rburksa@gmail.com>
2023-12-24 12:57:12 +0200

    Added PD2.5

--- PD2/PD2.5/src/lt/techin/rolandas/Main.java
+++ PD2/PD2.5/src/lt/techin/rolandas/Main.java
@@ -12,7 +12,7 @@ public class Main {      // Sukuriama klase Main
         Scanner sc = new Scanner(System.in);   // Inicijuojamas skaneris kuris yra naujas Skaneris ir zymimas sc
         /* Ekrane(informacijos isvedimo irenginyje) spausdiname teksta */
         System.out.print("Iveskite savo varda: ");
-/* Priskirame vartotojo ivesta teksta stringui userName */
+/* Priskirame vartotojo ivesta teksta stringui - userName */
         String userName = sc.nextLine();
 /* Isvedame vartotojo ivesta teksta pridedami reikiamus simbolius ir tarpus */
         System.out.print("Sveiki, "+ userName +"!");
