dc3ab3afedfb4998ec0834061c5ebf987fd70d86
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2023-12-24 12:54:51 +0200

    Update Main.java
    Pataisytos gramatines klaidos komentaruose

--- PD2/PD2.5/src/lt/techin/rolandas/Main.java
+++ PD2/PD2.5/src/lt/techin/rolandas/Main.java
@@ -1,7 +1,7 @@
 package lt.techin.rolandas;
 /* Importuojama klase Scanner tam kad
-kad butu galima iverti duomenis is
-ivedimo irenginio siuo atveju klaviaturos
+butu galima ivesti duomenis is
+ivedimo irenginio, siuo atveju klaviaturos
  */
 import java.util.Scanner;
 
@@ -10,9 +10,9 @@ public class Main {      // Sukuriama klase Main
     public static void main(String[] args) {    // Sukuriamas klases Main metodas
 
         Scanner sc = new Scanner(System.in);   // Inicijuojamas skaneris kuris yra naujas Skaneris ir zymimas sc
-        /* Ekrane(informacijos isvedimo irenginyje) spausdiname teksta */
+/* Ekrane(informacijos isvedimo irenginyje) spausdiname teksta */
         System.out.print("Iveskite savo varda: ");
-/* Priskirame vartotojo ivesta teksta stringuo userName */
+/* Priskirame vartotojo ivesta teksta stringui - userName */
         String userName = sc.nextLine();
 /* Isvedame vartotojo ivesta teksta pridedami reikiamus simbolius ir tarpus */
         System.out.print("Sveiki, "+ userName +"!");
