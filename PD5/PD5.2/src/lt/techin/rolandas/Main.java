967c931de5526c532b1a768b0691ed401703e72b
RolandasBurksa <rburksa@gmail.com>
2024-01-02 21:40:25 +0200

    Added PD5.2

---
+++ PD5/PD5.2/src/lt/techin/rolandas/Main.java
@@ -0,0 +1,15 @@
+package lt.techin.rolandas;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for ( int i = 1; i <= 10; i++) {                // Formuojame isorini cikla
+            for ( int j = 1; j <= 10; j++) {            // Formuojame vidini cikla
+                    System.out.println(i + " * " + j + " = " + (i * j));  // Spausdiname rezultatus
+                }
+            }
+        }
+
+    }
+
