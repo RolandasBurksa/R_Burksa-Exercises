611fb1fd2e648f58fb5d3351b04951f22a333479
RolandasBurksa <rburksa@gmail.com>
2024-01-04 15:45:12 +0200

    Added PD3.10

--- PD3/PD3.10/src/lt/techin/rolandas/Main.java
+++ PD3/PD3.10/src/lt/techin/rolandas/Main.java
@@ -4,28 +4,26 @@ import java.util.*;
 
 public class Main {
 
-
     public static void main(String[] args) {
-
+/*
+Ivedame duomenis
+ */
         Scanner sc = new Scanner(System.in);
         System.out.print("Iveskite sveiku skaiciu seka, atskirdami reiksmas tarpais: ");
         String str = sc.nextLine();
-
-//        int i = scan.nextInt();
-
+        System.out.println("Jus ivedete: " + str);  // Ivedimo kontrole
+/*
+Daliname/splitiname gauta Stringa pagal nurodyta pozymi, formuojame reiksmiu masyva
+ */
         String[] arrayString = str.split(" ");
 
-//        int[] myStringArr = new int[arrayString.length];
-
-       int a = Integer.parseInt(str);
-
-        System.out.println("Jus ivedete : " + str + " a = " + a);
-//        System.out.println("Jus ivedete : " + i);
-//        System.out.println("Jus ivedete : " + myStringArr);
-
-
-
-
+        int suma = 0;
+        for(int i = 0; i < arrayString.length; i++){  // Skaiciuojame masyvo reiksmiu suma
+          suma += Integer.parseInt(arrayString[i]);
+        }
+/*
+Spausdiname rezultata
+ */
+        System.out.println("Ivestu skaiciu suma = " + suma);
     }
-
 }
