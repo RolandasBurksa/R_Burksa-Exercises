86a5faf50af39b8d32784f7ab5a82d5eb1c0e53a
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-03-31 00:19:03 +0200

    Udemy 6 done

---
+++ Udemy Projects/src/lt/udemy/rolandas/section06/HelloWorld.java
@@ -0,0 +1,8 @@
+package lt.udemy.rolandas.section06;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Udemy Projects/src/lt/udemy/rolandas/section06/MultiplicationTableRunner.java
@@ -0,0 +1,13 @@
+package lt.udemy.rolandas.section06;
+
+public class MultiplicationTableRunner {
+
+    public static void main(String[] args) {
+
+        MultiplicationTable table = new MultiplicationTable();
+
+        table.print();
+//        table.print(18);
+//        table.print(4, 1, 20);
+    }
+}
