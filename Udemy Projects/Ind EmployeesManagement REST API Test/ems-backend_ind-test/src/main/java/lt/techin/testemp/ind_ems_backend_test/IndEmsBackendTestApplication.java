d23dee4e71965e02c1e11333e4b7677547050e95
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-06-22 14:16:47 +0300

    Backend initialization commit

---
+++ Udemy Projects/Ind EmployeesManagement REST API Test/ems-backend_ind-test/src/main/java/lt/techin/testemp/ind_ems_backend_test/IndEmsBackendTestApplication.java
@@ -0,0 +1,13 @@
+package lt.techin.testemp.ind_ems_backend_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndEmsBackendTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndEmsBackendTestApplication.class, args);
+	}
+
+}
---
+++ Udemy Projects/Ind EmployeesManagement REST API Test/ems-backend_ind-test/src/test/java/lt/techin/testemp/ind_ems_backend_test/IndEmsBackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.testemp.ind_ems_backend_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndEmsBackendTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
