bc2402cd6a04f2444805aac099f773975107eb86
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-06-22 20:04:22 +0300

    Front-end: Initialization commit

---
+++ Udemy Projects/Ind EmployeesManagement REST API Test/ems-frontend_ind-test/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HelloWorld from "./HelloWorld";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World !.</h1>
+      <HelloWorld />
+    </>
+  );
+}
+
+export default App;
