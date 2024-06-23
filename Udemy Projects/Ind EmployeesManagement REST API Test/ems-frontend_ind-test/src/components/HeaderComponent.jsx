36a1b1c79aa31e9c52993280d04d02d5b5548604
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-06-23 10:34:41 +0300

    Front-end: Added Header ant Footer Components

---
+++ Udemy Projects/Ind EmployeesManagement REST API Test/ems-frontend_ind-test/src/components/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+const HeaderComponent = () => {
+  return (
+    <div>
+      <header>
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="https://www.techin.lt">
+            Employee Management System
+          </a>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default HeaderComponent;
