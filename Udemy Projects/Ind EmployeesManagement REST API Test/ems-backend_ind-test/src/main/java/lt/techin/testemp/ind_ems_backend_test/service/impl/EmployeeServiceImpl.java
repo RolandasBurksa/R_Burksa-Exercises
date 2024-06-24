5529490936cd77b13c655b8d54183a835ce0e81b
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-06-24 23:31:53 +0300

    Back-end: Added Many To One Relationship in Spring Boot

--- Udemy Projects/Ind EmployeesManagement REST API Test/ems-backend_ind-test/src/main/java/lt/techin/testemp/ind_ems_backend_test/service/impl/EmployeeServiceImpl.java
+++ Udemy Projects/Ind EmployeesManagement REST API Test/ems-backend_ind-test/src/main/java/lt/techin/testemp/ind_ems_backend_test/service/impl/EmployeeServiceImpl.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import lt.techin.testemp.ind_ems_backend_test.dto.EmployeeDto;
 import lt.techin.testemp.ind_ems_backend_test.exception.ResourceNotFoundException;
 import lt.techin.testemp.ind_ems_backend_test.mapper.EmployeeMapper;
+import lt.techin.testemp.ind_ems_backend_test.model.Department;
 import lt.techin.testemp.ind_ems_backend_test.model.Employee;
+import lt.techin.testemp.ind_ems_backend_test.repository.DepartmentRepository;
 import lt.techin.testemp.ind_ems_backend_test.repository.EmployeeRepository;
 import lt.techin.testemp.ind_ems_backend_test.service.EmployeeService;
 import org.springframework.stereotype.Service;
@@ -18,10 +20,19 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
 
+    private DepartmentRepository departmentRepository;
+
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
 
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+
+        Department department = departmentRepository.findById(employeeDto.getDepartmentId())
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Department does not exist with id: " + employeeDto.getDepartmentId()));
+
+        employee.setDepartment(department);
+
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
     }
@@ -51,6 +62,12 @@ public class EmployeeServiceImpl implements EmployeeService {
        employee.setFirstName(updatedEmployee.getFirstName());
        employee.setLastName(updatedEmployee.getLastName());
        employee.setEmail(updatedEmployee.getEmail());
+
+        Department department = departmentRepository.findById(updatedEmployee.getDepartmentId())
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Department does not exist with id: " + updatedEmployee.getDepartmentId()));
+
+        employee.setDepartment(department);
 
       Employee updatedEmployeeObj = employeeRepository.save(employee);
 
