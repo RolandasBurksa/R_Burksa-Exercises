83fd723bb57fc0c3a98e52c8a9deeb046dece9b0
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-06-25 15:28:16 +0300

    Back-end: Added Role Based Authorization

--- Udemy Projects/Ind EmployeesManagement REST API Test/ems-backend_ind-test/src/main/java/lt/techin/testemp/ind_ems_backend_test/config/SpringSecurityConfig.java
+++ Udemy Projects/Ind EmployeesManagement REST API Test/ems-backend_ind-test/src/main/java/lt/techin/testemp/ind_ems_backend_test/config/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package lt.techin.testemp.ind_ems_backend_test.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -25,6 +26,11 @@ public class SpringSecurityConfig {
 
         http.csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests((authorize) -> {
+                    authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "USER");
+//                    authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll();
                     authorize.anyRequest().authenticated();
                 }).httpBasic(Customizer.withDefaults());
         return http.build();
