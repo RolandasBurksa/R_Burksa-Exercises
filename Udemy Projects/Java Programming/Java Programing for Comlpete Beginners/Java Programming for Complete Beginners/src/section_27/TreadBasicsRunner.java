d22555e9441f2b058f3f8ab404b3d4b7c96a0cd1
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-08-30 20:33:31 +0300

    Added some code

--- Udemy Projects/Java Programming/Java Programing for Comlpete Beginners/Java Programming for Complete Beginners/src/section_27/TreadBasicsRunner.java
+++ Udemy Projects/Java Programming/Java Programing for Comlpete Beginners/Java Programming for Complete Beginners/src/section_27/TreadBasicsRunner.java
@@ -1,4 +1,24 @@
 package section_27;
 
 public class TreadBasicsRunner {
+
+    public static void main(String[] args) {
+        //Task 1
+        for (int i = 101; i < 199; i++) {
+            System.out.println(i + " ");
+        }
+        System.out.println("\n Task1 Done");
+        //Task 2
+        for (int i = 201; i < 299; i++) {
+            System.out.println(i + " ");
+        }
+        System.out.println("\n Task2 Done");
+        //Task 3
+        for (int i = 301; i < 399; i++) {
+            System.out.println(i + " ");
+        }
+        System.out.println("\n Task3 Done");
+        System.out.println("---   ---   ---");
+        System.out.println("\n The Main method is Done");
+    }
 }
