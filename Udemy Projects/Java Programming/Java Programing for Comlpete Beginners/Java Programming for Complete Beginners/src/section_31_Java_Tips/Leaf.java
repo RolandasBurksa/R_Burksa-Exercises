df6634a0f7018e14166a17471b3b2dc521d826c6
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-09-03 16:58:08 +0300

    Added some code

---
+++ Udemy Projects/Java Programming/Java Programing for Comlpete Beginners/Java Programming for Complete Beginners/src/section_31_Java_Tips/Leaf.java
@@ -0,0 +1,18 @@
+package section_31_Java_Tips;
+
+public class Leaf {
+    int i = 0;
+    Leaf increment() {
+        i++;
+        return this;
+    }
+
+    void print() {
+        System.out.println("i = " + i);
+    }
+
+    public static void main(String[] args) {
+        Leaf x = new Leaf();
+        x.increment().increment().increment().print();
+    }
+}
