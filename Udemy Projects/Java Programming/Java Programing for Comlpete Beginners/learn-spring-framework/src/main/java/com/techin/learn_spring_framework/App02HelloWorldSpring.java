ec2c2ad1b2e47debc98a9c32ba6819a8300f362b
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-09-12 19:11:01 +0300

    Added some code

--- Udemy Projects/Java Programming/Java Programing for Comlpete Beginners/learn-spring-framework/src/main/java/com/techin/learn_spring_framework/App02HelloWorldSpring.java
+++ Udemy Projects/Java Programming/Java Programing for Comlpete Beginners/learn-spring-framework/src/main/java/com/techin/learn_spring_framework/App02HelloWorldSpring.java
@@ -4,6 +4,8 @@ import com.techin.learn_spring_framework.game.GameRunner;
 import com.techin.learn_spring_framework.game.PacManGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class App02HelloWorldSpring {
 
     public static void main(String[] args) {
@@ -24,7 +26,12 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("person3Parameters"));
 
         System.out.println(context.getBean("address2"));
-//        System.out.println(context.getBean(Address.class));
+        System.out.println(context.getBean(Person.class));
+        System.out.println(context.getBean(Address.class));
+
+        System.out.println(context.getBean("person5Qualifier"));
+
+//        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 
     }
 }
