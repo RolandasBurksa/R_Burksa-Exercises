e2dbed08aedb063be2916ffd62eabac272a2dc27
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-09-23 20:59:05 +0300

    Added some code

---
+++ Udemy Projects/Spring Boot 3, Spring 6 & Hibernate for Beginners/dev-spring-boot/02-spring-boot-spring-core/04-qualifiers/src/main/java/com/techin/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,12 @@
+package com.techin.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practise your backhand volley..";
+    }
+}
