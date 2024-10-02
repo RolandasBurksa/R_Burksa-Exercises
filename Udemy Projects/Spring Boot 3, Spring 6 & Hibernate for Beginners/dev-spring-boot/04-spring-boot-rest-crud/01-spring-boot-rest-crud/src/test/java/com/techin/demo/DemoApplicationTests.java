93fb56d818c6b5f3e3ba97be696eb3f782bcec07
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-10-02 20:16:36 +0300

    Added new spring-boot-rest-crud project folder

---
+++ Udemy Projects/Spring Boot 3, Spring 6 & Hibernate for Beginners/dev-spring-boot/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/test/java/com/techin/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
