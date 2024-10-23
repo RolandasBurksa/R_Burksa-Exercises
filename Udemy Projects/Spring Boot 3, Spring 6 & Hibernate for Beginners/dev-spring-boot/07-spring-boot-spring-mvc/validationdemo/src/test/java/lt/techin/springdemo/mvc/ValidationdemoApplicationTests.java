6be0a86c0c86a09bef40005e27bc53255d35c627
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-10-23 22:14:30 +0300

    Added new project

---
+++ Udemy Projects/Spring Boot 3, Spring 6 & Hibernate for Beginners/dev-spring-boot/07-spring-boot-spring-mvc/validationdemo/src/test/java/lt/techin/springdemo/mvc/ValidationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.springdemo.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
