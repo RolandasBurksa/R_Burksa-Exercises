73783eb491780cc46ecd0c0030936a02113f0e7a
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-11-15 19:48:43 +0200

    Added new part of project 04

---
+++ Udemy Projects/Spring Boot 3, Spring 6 & Hibernate for Beginners/dev-spring-boot/09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/techin/cruddemo/CruddemoApplication.java
@@ -0,0 +1,253 @@
+package com.techin.cruddemo;
+
+import com.techin.cruddemo.dao.AppDAO;
+import com.techin.cruddemo.entity.Course;
+import com.techin.cruddemo.entity.Instructor;
+import com.techin.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+
+		return runner -> {
+			// createInstructor(appDAO);
+
+			// findInstructor(appDAO);
+
+			// deleteInstructor(appDAO);
+
+			// findInstructorDetail(appDAO);
+
+			// deleteInstructorDetail(appDAO);
+
+			// createInstructorWithCourses(appDAO);
+
+			// findInstructorWithCourses (appDAO);
+			
+			// findCoursesForInstructor(appDAO);
+
+			// findInstructorWithCoursesJoinFetch(appDAO);
+
+			// updateInstructor(appDAO);
+
+			// updateCourse(appDAO);
+
+			// deleteInstructor(appDAO);
+
+			deleteCourse(appDAO);
+		};
+	}
+
+	private void deleteCourse(AppDAO appDAO) {
+
+		int theId = 10;
+
+		System.out.println("Deleting course id: " + theId);
+
+		appDAO.deleteCourseById(theId);
+
+		System.out.println("Done, Course deleted!");
+	}
+
+	private void updateCourse(AppDAO appDAO) {
+
+		int theId = 10;
+
+		// find the course
+		System.out.println("Finding course id: " + theId);
+		Course tempCourse = appDAO.findCourseById(theId);
+
+		System.out.println("Done!..");
+
+		// update the course
+		System.out.println("Updating the course id: " + theId);
+		tempCourse.setTitle("Enjoy the Simple Things!.");
+
+		appDAO.update(tempCourse);
+
+		System.out.println("Done!..");
+
+	}
+
+	private void updateInstructor(AppDAO appDAO) {
+
+		int theId= 1;
+
+		// find the instructor
+		System.out.println("Finding instructor id: " + theId);
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		// update thr instructor
+		System.out.println("Updating instructor id: " + theId);
+		tempInstructor.setLastName("TESTER");
+
+		appDAO.update(tempInstructor);
+
+		System.out.println("Done!.");
+
+	}
+
+	private void findInstructorWithCoursesJoinFetch(AppDAO appDAO) {
+
+		int theId = 1;
+
+		// find the instructor
+		System.out.println("Finding instructor id: " + theId);
+		Instructor tempInstructor = appDAO.findInstructorByIdJoinFetch(theId);
+
+		System.out.println("tempInstructor; " + tempInstructor);
+		System.out.println("the associate courses: " + tempInstructor.getCourses());
+
+		System.out.println("Done!.");
+	}
+
+	private void findCoursesForInstructor(AppDAO appDAO) {
+
+		int theId = 1;
+		// find instructor
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+
+		// find courses for instructor
+		System.out.println("Finding courses for instructor id: " + theId);
+		List<Course> courses =appDAO.findCoursesByInstructorID(theId);
+
+		// associate the objects
+		tempInstructor.setCourses(courses);
+
+		System.out.println("the associated courses: " + tempInstructor.getCourses());
+
+		System.out.println("Done!");
+
+	}
+
+	private void findInstructorWithCourses(AppDAO appDAO) {
+
+		int theId = 1;
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associated courses: " + tempInstructor.getCourses());
+
+		System.out.println("Done!.");
+	}
+
+	private void createInstructorWithCourses(AppDAO appDAO) {
+
+		// create the instructor
+		Instructor tempInstructor = new Instructor("Susan", "SusanBest", "susan@gmail.com");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.spark-fun/youtube", "Jogging...");
+
+		// associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// create some courses
+		Course tempCourse1 = new Course("Jogging for everyone");
+		Course tempCourse2 = new Course("Swimming for advanced swimmers");
+
+		// add courses for instructor
+		tempInstructor.add(tempCourse1);
+		tempInstructor.add(tempCourse2);
+
+		// saving the instructor
+		//
+		// NOTE: this will ALSO save the courses because of CascadeType.PERSIST
+		System.out.println("Saving instructor: " + tempInstructor);
+		System.out.println("The courses: " + tempInstructor.getCourses());
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!.");
+		
+	}
+
+	private void deleteInstructorDetail(AppDAO appDAO) {
+
+		int theId = 4;
+		System.out.println("deleting instructor detail id: "+ theId);
+
+		appDAO.deleteInstructorDetailById(theId);
+
+		System.out.println("Done instructor+instructorDetails deleted!.");
+	}
+
+	private void findInstructorDetail(AppDAO appDAO) {
+
+		// get the instructor detail object
+		int theId = 3;
+		InstructorDetail tempInstructorDetail = appDAO.findInstructorDetailById(theId);
+
+		// print the instructor detail
+		System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+
+		// print the associated instructor
+		System.out.println("the associated instructor: " + tempInstructorDetail.getInstructor());
+
+		System.out.println("Done with find-instructor!.");
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+
+		int theId = 1;
+		System.out.println("Delete instructor id: " + theId);
+
+		appDAO.deleteInstructorById(theId);
+
+		System.out.println("Done, instructor deleted!.");
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+
+		int theId = 2;
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associated instructorDetail only: " + tempInstructor.getInstructorDetail());
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+/*
+		// create the instructor
+		Instructor tempInstructor = new Instructor("Algis", "Algiauskas", "algis@gmail.com");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.gymshark/youtube", "swimming...");
+*/
+		// create the instructor
+		Instructor tempInstructor = new Instructor("Dalgis", "Dalgiauskas", "dalgis@gmail.com");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.shark-spark/youtube", "running...");
+
+		// associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// save the instructor
+		// NOTE: this ALSO save the details object
+		// because of CascadeType.ALL
+		System.out.println("Saving instructor: " + tempInstructor);
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!.");
+	}
+}
---
+++ Udemy Projects/Spring Boot 3, Spring 6 & Hibernate for Beginners/dev-spring-boot/09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/techin/cruddemo/dao/AppDAO.java
@@ -0,0 +1,32 @@
+package com.techin.cruddemo.dao;
+
+import com.techin.cruddemo.entity.Course;
+import com.techin.cruddemo.entity.Instructor;
+import com.techin.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+
+    void save(Instructor instructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+    List<Course> findCoursesByInstructorID(int theId);
+
+    Instructor findInstructorByIdJoinFetch(int theId);
+
+    void update(Instructor tempInstructor);
+
+    void update(Course tempCourse);
+
+    Course findCourseById(int theId);
+
+    void deleteCourseById(int theId);
+}
