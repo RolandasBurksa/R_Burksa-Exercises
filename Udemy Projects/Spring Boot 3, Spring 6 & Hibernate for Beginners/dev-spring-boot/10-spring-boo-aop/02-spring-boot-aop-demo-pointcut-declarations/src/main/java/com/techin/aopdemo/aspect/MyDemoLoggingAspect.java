0e775189fdee6415e6ba57b01a9a0922ffd03059
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-11-22 18:59:15 +0200

    Added some code

--- Udemy Projects/Spring Boot 3, Spring 6 & Hibernate for Beginners/dev-spring-boot/10-spring-boo-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/techin/aopdemo/aspect/MyDemoLoggingAspect.java
+++ Udemy Projects/Spring Boot 3, Spring 6 & Hibernate for Beginners/dev-spring-boot/10-spring-boo-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/techin/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.techin.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -12,9 +13,29 @@ public class MyDemoLoggingAspect {
 
     // let's start with a @Before advice
 
-    @Before("execution(* com.techin.aopdemo.dao.*.*(..))")
+    @Pointcut("execution(* com.techin.aopdemo.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    // create a pointcut for getter methods
+    @Pointcut("execution(* com.techin.aopdemo.dao.*.get*(..))")
+    private void getter() {}
+
+    // create a pointcut for setter methods
+    @Pointcut("execution(* com.techin.aopdemo.dao.*.set*(..))")
+    private void setter() {}
+
+    // create a pointcut: include package ... exclude getter/setter
+    @Pointcut("forDaoPackage() && !( getter() || setter())")
+    private void forDaoPackageNoGetterSetter() {}
+
+    @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
 
         System.out.println("\n=========>>>>> Executing @Before advice on addSillyMember()");
+    }
+
+    @Before("forDaoPackageNoGetterSetter()")
+    public void performApiAnalytics() {
+        System.out.println("\n=========>>>>> Performing API analytics");
     }
 }
