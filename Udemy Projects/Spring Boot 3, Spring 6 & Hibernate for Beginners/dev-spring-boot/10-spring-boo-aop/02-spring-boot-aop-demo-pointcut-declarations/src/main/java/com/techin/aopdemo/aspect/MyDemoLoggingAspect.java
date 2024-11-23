d3ee881bd4c75e060199c9c66b57b32468bc82bb
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-11-23 19:41:24 +0200

    Added arguments with JoinPoints

--- Udemy Projects/Spring Boot 3, Spring 6 & Hibernate for Beginners/dev-spring-boot/10-spring-boo-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/techin/aopdemo/aspect/MyDemoLoggingAspect.java
+++ Udemy Projects/Spring Boot 3, Spring 6 & Hibernate for Beginners/dev-spring-boot/10-spring-boo-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/techin/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,7 +1,10 @@
 package com.techin.aopdemo.aspect;
 
+import com.techin.aopdemo.Account;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +14,31 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 
     @Before("com.techin.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
-
+    public void beforeAddAccountAdvice(JoinPoint theJoinpoint) {
         System.out.println("\n=========>>>>> Executing @Before advice on addSillyMember()");
+
+        // display the method signature
+        MethodSignature methodSignature = (MethodSignature) theJoinpoint.getSignature();
+
+        System.out.println("Method: " + methodSignature);
+
+        // display method arguments
+
+        // get args
+        Object[] args = theJoinpoint.getArgs();
+
+        // loop thru args
+        for (Object tempArg : args) {
+            System.out.println(tempArg);
+
+            if (tempArg instanceof Account) {
+
+                // downcast and print Account specific stuff
+                Account theAccount = (Account) tempArg;
+
+                System.out.println("account name: " + theAccount.getName());
+                System.out.println("account name: " + theAccount.getLevel());
+            }
+        }
     }
 }
