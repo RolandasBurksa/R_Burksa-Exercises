832d946a5fa4761b922525de6de90aa263ffbda2
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-11-24 15:49:31 +0200

    Added the Around Advice

--- Udemy Projects/Spring Boot 3, Spring 6 & Hibernate for Beginners/dev-spring-boot/10-spring-boo-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/techin/aopdemo/aspect/MyDemoLoggingAspect.java
+++ Udemy Projects/Spring Boot 3, Spring 6 & Hibernate for Beginners/dev-spring-boot/10-spring-boo-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/techin/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,10 +2,8 @@ package com.techin.aopdemo.aspect;
 
 import com.techin.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -16,6 +14,30 @@ import java.util.List;
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+
+    @Around("execution(* com.techin.aopdemo.service.*.getFortune(..))")
+    public Object aroundGetFortune(
+            ProceedingJoinPoint theProceedingJoinPoint) throws Throwable {
+
+        // print out method we are advising on
+        String method = theProceedingJoinPoint.getSignature().toShortString();
+        System.out.println("\n==========>>>>>> Executing @Around on method: " + method);
+
+        // get begin timestamp
+        long begin = System.currentTimeMillis();
+
+        // now, let's execute the method
+        Object result = theProceedingJoinPoint.proceed();
+
+        // get end time stamp
+        long end = System.currentTimeMillis();
+
+        // compute duration and display it
+        long duration = end - begin;
+        System.out.println("\n==========>>>>> Duration: " + duration / 1000.0 + " seconds");
+
+        return result;
+    }
 
     @After("execution(* com.techin.aopdemo.dao.AccountDAO.findAccounts(..))")
     public void afterFinallyFindAccountsAdvice(JoinPoint theJoinPoint) {
---
+++ Udemy Projects/Spring Boot 3, Spring 6 & Hibernate for Beginners/dev-spring-boot/10-spring-boo-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/techin/aopdemo/service/TrafficFortuneService.java
@@ -0,0 +1,6 @@
+package com.techin.aopdemo.service;
+
+public interface TrafficFortuneService {
+
+    String getFortune();
+}
