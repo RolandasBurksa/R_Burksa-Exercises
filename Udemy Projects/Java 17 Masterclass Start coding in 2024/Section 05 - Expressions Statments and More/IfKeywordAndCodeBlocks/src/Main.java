ba8897ec1351222545ff8403a3c36effae314964
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-11-28 23:45:07 +0200

    Added some code

--- Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 05 - Expressions Statments and More/IfKeywordAndCodeBlocks/src/Main.java
+++ Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 05 - Expressions Statments and More/IfKeywordAndCodeBlocks/src/Main.java
@@ -2,5 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score < 5000 && score > 1000) {
+            System.out.println("1 - Your score was: " + score);
+        } else if (score < 1000) {
+            System.out.println("2 - Your score is less then 1000, score: " + score );
+        } else {
+            System.out.println("3 - Got there!.");
+        }
     }
 }
