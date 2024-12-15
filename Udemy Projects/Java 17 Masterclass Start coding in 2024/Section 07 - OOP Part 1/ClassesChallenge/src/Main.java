fdb05e733d7302d85652f33c6d560387e58e17ba
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-12-15 20:23:22 +0200

    Added some solutions

---
+++ Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 07 - OOP Part 1/ClassesChallenge/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Account bobsAccount = new Account();
+        bobsAccount.withdrawalFunds(100.0);
+        bobsAccount.depositFunds(250.0);
+        bobsAccount.withdrawalFunds(50.0);
+
+    }
+}
