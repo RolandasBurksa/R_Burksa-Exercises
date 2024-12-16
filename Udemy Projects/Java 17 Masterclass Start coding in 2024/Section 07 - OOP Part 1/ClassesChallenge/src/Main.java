9de9b20426c99fe28d9f726f639cb94623c90de2
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-12-17 00:00:31 +0200

    Added some classes

--- Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 07 - OOP Part 1/ClassesChallenge/src/Main.java
+++ Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 07 - OOP Part 1/ClassesChallenge/src/Main.java
@@ -3,9 +3,19 @@ public class Main {
     public static void main(String[] args) {
 
         Account bobsAccount = new Account();
+
+        Account marysAccount = new Account("12345678", 10000.0, "Mary Dary", "marys@gmail.com", "+370-123-12345");
+
         bobsAccount.withdrawalFunds(100.0);
         bobsAccount.depositFunds(250.0);
         bobsAccount.withdrawalFunds(50.0);
+
+        marysAccount.withdrawalFunds(150.0);
+        marysAccount.depositFunds(350.0);
+        marysAccount.withdrawalFunds(550.0);
+
+        System.out.println(marysAccount.getNumber());
+        System.out.println(marysAccount.getBalance());
 
     }
 }
