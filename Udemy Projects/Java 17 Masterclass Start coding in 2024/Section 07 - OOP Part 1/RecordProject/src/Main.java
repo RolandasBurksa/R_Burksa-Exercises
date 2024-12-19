d6dc23d60f2ea21283c08b0ea691dafa59b9ee6b
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-12-19 23:20:46 +0200

    Added some code

--- Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 07 - OOP Part 1/RecordProject/src/Main.java
+++ Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 07 - OOP Part 1/RecordProject/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i =1; i <= 5; i++) {
-            Student s = new Student("S92300" + i,
+            LPAStudent s = new LPAStudent("S92300" + i,
                     switch (i) {
                         case 1 -> "Mary";
                         case 2 -> "Carol";
@@ -14,6 +14,31 @@ public class Main {
                     },
                                 "01/01/1991",
                                 "Java Masterclass");
+
+            System.out.println(s);
+        }
+
+        Student pojoStudent = new Student("S923006", "Ann", "05/10/1985", "Java Masterclass");
+        LPAStudent recordStudent = new LPAStudent("S923007", "Bill", "05/10/1985", "Java Masterclass");
+
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " + recordStudent.classList());
+
+        for (int i =1; i <= 5; i++) {
+            Student s = new Student("S92300" + i,
+                    switch (i) {
+                        case 1 -> "Mary";
+                        case 2 -> "Carol";
+                        case 3 -> "Harry";
+                        case 4 -> "Lisa";
+                        case 5 -> "Tim";
+                        default -> "Anonymous";
+                    },
+                    "01/01/1991",
+                    "Java Masterclass");
 
             System.out.println(s);
         }
