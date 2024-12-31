335d3806f9aa781b05167a85b9fb45eaa38798c6
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-12-31 21:04:25 +0200

    Added some code

--- Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 08 - OOP Part 2/PolymorphismPartOne/src/Main.java
+++ Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 08 - OOP Part 2/PolymorphismPartOne/src/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,7 +9,23 @@ public class Main {
 //        Movie theMovie = new Movie("Star Wars");
 //        theMovie.watchMovie();
 
-        Movie theMovie = new Adventure("Star Wars");
-        theMovie.watchMovie();
+//        Movie theMovie = new Adventure("Star Wars");
+//        theMovie.watchMovie();
+
+//        Movie theMovie = Movie.getMovie("Adventure", "Star Wars");
+//        theMovie.watchMovie();
+
+        Scanner s = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter movie type A, C, S or Q to quit: ");
+            String type = s.nextLine();
+            if ("Qq".contains(type)) {
+                break;
+            }
+            System.out.println("Enter Movie Title: ");
+            String title = s.nextLine();
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
     }
 }
