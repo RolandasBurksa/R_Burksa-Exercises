088a2fb4f655f4e732320e55d21fb16a462c57a6
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2025-01-03 20:02:08 +0200

    Added completed task

--- Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 08 - OOP Part 2/FoodRestaurant/src/Main.java
+++ Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 08 - OOP Part 2/FoodRestaurant/src/Main.java
@@ -2,15 +2,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Item coke = new Item("drink", "coke", 1.5);
-        coke.printItem();
-        coke.setSize("LARGE");
-        coke.printItem();
+//        Item coke = new Item("drink", "coke", 1.5);
+//        coke.printItem();
+//        coke.setSize("LARGE");
+//        coke.printItem();
+//
+//        Item abc = new Item("avocado", "avocado", 1.5);
+//        abc.printItem();
+//
+//        Item def = new Item("side", "nothing", 2.0);
+//        def.printItem();
 
-        Item abc = new Item("avocado", "avocado", 1.5);
-        abc.printItem();
+//        Burger burger = new Burger("regular", 4.00);
+//        burger.addToppings("BACON", "CHEESE", "MAYO");
+//        burger.printItem();
 
-        Item def = new Item("side", "nothing", 2.0);
-        def.printItem();
+//        Order regularMeal = new Order();
+//        regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+//        regularMeal.setDrinkSize("LARGE");
+//        regularMeal.printItemizedList();
+
+        Order secondMeal = new Order("turkey", "7-up", "chili");
+        secondMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+        secondMeal.setDrinkSize("SMALL");
+        secondMeal.printItemizedList();
     }
 }
