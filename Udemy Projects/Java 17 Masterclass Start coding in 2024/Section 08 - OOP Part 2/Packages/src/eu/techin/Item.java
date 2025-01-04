454989fceae1c43be237db4efb52b1eabc26b00f
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2025-01-04 23:09:52 +0200

    Added some tasks

---
+++ Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 08 - OOP Part 2/Packages/src/eu/techin/Item.java
@@ -0,0 +1,17 @@
+package eu.techin;
+
+public class Item {
+
+    private String type;
+
+    public Item(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "type='" + type + '\'' +
+                '}';
+    }
+}
---
+++ Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 08 - OOP Part 2/Packages/src/lt/techin/Main.java
@@ -0,0 +1,14 @@
+package lt.techin;
+
+import eu.techin.Item;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Item firstItem = new Item("Burger");
+
+        eu.techin.Item firstItem = new eu.techin.Item("Burger");
+        System.out.println(firstItem);
+    }
+}
