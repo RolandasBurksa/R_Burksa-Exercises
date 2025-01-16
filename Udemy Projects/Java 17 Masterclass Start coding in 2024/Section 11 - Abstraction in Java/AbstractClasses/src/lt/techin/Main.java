63b74c01f8710d80186407d5c646a9a3f388847c
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2025-01-16 13:11:46 +0200

    Added some code

--- Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 11 - Abstraction in Java/AbstractClasses/src/lt/techin/Main.java
+++ Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 11 - Abstraction in Java/AbstractClasses/src/lt/techin/Main.java
@@ -1,8 +1,34 @@
 package lt.techin;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
+//        Animal animal = new Animal("animal", "big", 100);
+
+        Dog dog = new Dog("Wolf", "big", 100);
+        dog.makeNoise();
+        doAnimalStuff(dog);
+
+        System.out.println("-".repeat(30));
+
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(dog);
+        animals.add(new Dog("German Shepard", "big", 150));
+        animals.add(new Fish("Goldfish", "small", 1));
+        animals.add(new Fish("Barracuda", "big", 75));
+        animals.add(new Dog("Pug", "small", 20));
+
+        for (Animal animal : animals) {
+            doAnimalStuff(animal);
+        }
+    }
+
+    private static void doAnimalStuff(Animal animal) {
+
+        animal.makeNoise();
+        animal.move("slow");
     }
 }
