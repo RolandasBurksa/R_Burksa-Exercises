3e3c0028716b0d96b2515c2bbe4092fc1a5b6445
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2025-01-19 00:26:30 +0200

    Added some code

--- Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 11 - Abstraction in Java/Interfaces/src/lt/techin/Test.java
+++ Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 11 - Abstraction in Java/Interfaces/src/lt/techin/Test.java
@@ -5,6 +5,10 @@ public class Test {
     public static void main(String[] args) {
 
         inFlight(new Jet());
+
+//        OrbitEarth.log("Testing " + new Satellite());
+        orbit(new Satellite());
+
     }
 
         private static void inFlight(FlightEnabled flier) {
@@ -17,5 +21,12 @@ public class Test {
             }
             flier.land();
         }
+
+    private static void orbit(OrbitEarth flier) {
+
+        flier.takeOff();
+        flier.fly();
+        flier.land();
+    }
 
 }
