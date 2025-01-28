848a9858e83b3728c05be33ab60954989c61375a
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2025-01-28 23:04:30 +0200

    Update Main.java

--- Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 14 - Lambda Expressions, Functional Interfaces, and Method References/LambdaChallOne/src/lt/techin/Main.java
+++ Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 14 - Lambda Expressions, Functional Interfaces, and Method References/LambdaChallOne/src/lt/techin/Main.java
@@ -2,6 +2,9 @@ package lt.techin;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 public class Main {
 
@@ -40,5 +43,42 @@ public class Main {
         };
 
         printWordsConcise.accept("Let's split up into an array");
+
+        UnaryOperator<String> everySecondChar = source -> {
+            StringBuilder returnVal = new StringBuilder();
+            for (int i = 0; i < source.length(); i++) {
+                if (i % 2 == 1) {
+                    returnVal.append(source.charAt(i));
+                }
+            }
+            return returnVal.toString();
+        };
+
+        System.out.println(everySecondChar.apply("1234567890"));
+
+        String result = everySecondCharacter(everySecondChar, "1234567890");
+        System.out.println(result);
+
+        Supplier<String> iLoveJava = () -> "I love Java!";
+        Supplier<String> iLoveJava2 = () -> {return "I love Java!";};
+
+        System.out.println(iLoveJava.get());
+        System.out.println(iLoveJava2.get());
+
+    }
+
+    public static String everySecondChar(String source) {
+
+        StringBuilder returnVal = new StringBuilder();
+        for (int i = 0; i < source.length(); i++) {
+            if (i % 2 == 1) {
+                returnVal.append(source.charAt(i));
+            }
+        }
+        return returnVal.toString();
+    }
+
+    public static String everySecondCharacter(Function<String, String> func, String source) {
+        return func.apply(source);
     }
 }
