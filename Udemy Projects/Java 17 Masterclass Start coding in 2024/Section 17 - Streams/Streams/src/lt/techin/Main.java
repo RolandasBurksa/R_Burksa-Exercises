1feb941971afb63c9121c4747b4a44f969e476fe
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2025-02-04 23:47:45 +0200

    Update Main.java

--- Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 17 - Streams/Streams/src/lt/techin/Main.java
+++ Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 17 - Streams/Streams/src/lt/techin/Main.java
@@ -1,6 +1,8 @@
 package lt.techin;
 
 import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -31,9 +33,9 @@ public class Main {
                 String updated = s.charAt(0) + "-" + s.substring(1);
                 System.out.print(updated + " ");
                 return updated;
-                    }
-                 return s;
-                });
+            }
+            return s;
+        });
         System.out.println("\n-------------------------------------");
 
         for (int i = 0; i < 15; i++) {
@@ -41,17 +43,89 @@ public class Main {
         }
         System.out.println("-------------------------------------");
 
-        bingoPool.stream()
+        var tempStream = bingoPool.stream()
                 .limit(15)
                 .filter(s -> s.indexOf('G') == 0 || s.indexOf("O") == 0)
                 .map(s -> s.charAt(0) + "-" + s.substring(1))
-                .sorted()
+                .sorted();
+//                .forEach(s -> System.out.print(s + " "));
+
+//        for (int i = 0; i < 15; i++) {
+//            System.out.println(bingoPool.get(i));
+//        }
+        tempStream.forEach(s -> System.out.print(s + " "));
+        System.out.println("\n-------------------------------------");
+
+        String[] strings = {"One", "Two", "Three"};
+        var firstStream = Arrays.stream(strings)
+                .sorted(Comparator.reverseOrder());
+//                .forEach(System.out::println);
+
+        var secondStream = Stream.of("Six", "Five", "Four")
+                .map(String::toUpperCase);
+//                .forEach(System.out::println);
+
+        Stream.concat(secondStream, firstStream)
+                .map(s -> s.charAt(0) + " - " + s)
+                .forEach(System.out::println);
+
+        Map<Character, int[]> myMap = new LinkedHashMap<>();
+        int bingoIndex = 1;
+        for (char c : "BINGO".toCharArray()) {
+            int[] numbers = new int[15];
+            int labelNo = bingoIndex;
+            Arrays.setAll(numbers, i -> i + labelNo);
+            myMap.put(c, numbers);
+            bingoIndex += 15;
+        }
+
+        myMap.entrySet()
+                .stream()
+                .map(e -> e.getKey() + " has range: " + e.getValue()[0] + " - " +
+                        e.getValue()[e.getValue().length - 1])
+                .forEach(System.out::println);
+
+        Random random = new Random();
+        Stream.generate(() -> random.nextInt(2))
+                .limit(10)
                 .forEach(s -> System.out.print(s + " "));
 
-        for (int i = 0; i < 15; i++) {
-            System.out.println(bingoPool.get(i));
-        }
-        System.out.println("-------------------------------------");
+        System.out.println();
+        IntStream.iterate(1, n -> n + 1)
+                .filter(Main::isPrime)
+                .limit(20)
+                .forEach(s -> System.out.print(s + " "));
 
+        System.out.println();
+        IntStream.iterate(1, n -> n + 1)
+                .limit(100)
+                .filter(Main::isPrime)
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+        IntStream.iterate(1, n -> n <= 100, n -> n + 1)
+                .filter(Main::isPrime)
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+        IntStream.rangeClosed(1, 100)
+                .filter(Main::isPrime)
+                .forEach(s -> System.out.print(s + " "));
     }
+
+        public static boolean isPrime(int wholeNumber) {
+
+            if (wholeNumber <= 2) {
+                return (wholeNumber == 2);
+            }
+
+            for (int divisor = 2; divisor < wholeNumber; divisor++) {
+                if (wholeNumber % divisor == 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
 }
