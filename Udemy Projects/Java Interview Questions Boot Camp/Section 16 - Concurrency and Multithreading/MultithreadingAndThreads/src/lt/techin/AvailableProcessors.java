cc1b236c38c9efd2a069b3a487ba769e4e821883
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2025-02-17 20:21:00 +0200

    Create AvailableProcessors.java

---
+++ Udemy Projects/Java Interview Questions Boot Camp/Section 16 - Concurrency and Multithreading/MultithreadingAndThreads/src/lt/techin/AvailableProcessors.java
@@ -0,0 +1,9 @@
+package lt.techin;
+
+public class AvailableProcessors {
+
+    public static void main(String[] args) {
+
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
