cc33f2bf9190da041eb0cd7b6791ffa56625418f
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2025-03-04 00:30:09 +0200

    Added JPA tasks

--- Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 22 - Working with Databases/JPA/src/lt/techin/Main.java
+++ Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 22 - Working with Databases/JPA/src/lt/techin/Main.java
@@ -1,8 +1,29 @@
 package lt.techin;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+import lt.techin.music.Artist;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        try (var sessionFactory =
+                     Persistence.createEntityManagerFactory("lt.techin.music");
+             EntityManager entityManager = sessionFactory.createEntityManager();
+             ) {
+
+            var transaction = entityManager.getTransaction();
+            transaction.begin();
+            Artist artist = entityManager.find(Artist.class, 202);
+            artist.setArtistName("Muddy Waters");
+            System.out.println(artist);
+//            entityManager.remove(artist);
+//            entityManager.persist(new Artist("Muddy Water"));
+            transaction.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
