42f5959a71e44b8b8dc018260ad0672b255a46d0
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2025-03-07 18:05:09 +0200

    Create LoggingToFile.java

---
+++ Udemy Projects/Java 17 Masterclass Start coding in 2024/Section 24 - Debugging and Testing in Java/DebuggerDemo/src/lt/techin/LoggingToFile.java
@@ -0,0 +1,32 @@
+package lt.techin;
+
+class Logger {
+    public void logInformation(String logInfo) {
+        System.out.println(logInfo);
+    }
+}
+
+class LoggingToFileA {
+        private Logger logger;
+
+    public LoggingToFileA(Logger logger) {
+            this.logger = logger;
+        }
+
+        public void logging() {
+            logger.logInformation("A B C");
+        }
+}
+
+class LoggingToFile {
+
+    public static void main(String[] args) {
+
+        LoggingToFileA fileLogger = new LoggingToFileA(new Logger());
+        fileLogger.logging();
+
+        Logger logger = new Logger();
+        logger.logInformation("a-b-c");
+
+    }
+}
