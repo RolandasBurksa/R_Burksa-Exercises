a92ff2ad7db74224684e6f69196002e528ea1836
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2025-03-17 23:27:13 +0200

    Added some code

---
+++ 2025_Codecacademy_tasks/01_Java_Variables/JavaVariablesMadLibs/src/lt/techin/Maths.java
@@ -0,0 +1,13 @@
+package lt.techin;
+
+public class Maths {
+
+    public Maths() {
+        System.out.println("I am constructor");
+    }
+    public static void main(String [] args) {
+
+        System.out.println("I am main");
+        Maths obj1 = new Maths();
+    }
+}
---
+++ 2025_Codecacademy_tasks/01_Java_Variables/JavaVariablesMadLibs/src/lt/techin/Person.java
@@ -0,0 +1,36 @@
+package lt.techin;
+
+public class Person {
+
+    // state of an object
+    int age;
+    String name;
+
+    // behavior of an object
+    public void set_value() {
+        age = 20;
+        name = "Robin";
+    }
+    public void get_value() {
+        System.out.println("Age is " + age);
+        System.out.println("Name is " + name);
+    }
+
+    // main method
+    public static void main(String [] args) {
+        // creates a new Person object
+        Person p = new Person();
+
+        // changes state through behavior
+        p.set_value();
+
+        // print the updated values
+        p.get_value();
+
+        p.age = 33;
+        p.name = "Bob";
+
+        p.get_value();
+
+    }
+}
