7aa69472692c3de0fde90e5e49f5a0beadb65033
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2025-03-28 01:19:54 +0200

    Added some tasks

---
+++ 2025_Codecacademy_tasks/01_Java_Variables/Scope/src/lt/techin/Main.java
@@ -0,0 +1,12 @@
+package lt.techin;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SavingsAccount zeusSavingsAccount = new SavingsAccount("Zeus", 1000);
+
+        // Make a call to addMoney() to test your method
+        zeusSavingsAccount.addMoney(222);
+    }
+}
---
+++ 2025_Codecacademy_tasks/01_Java_Variables/Scope/src/lt/techin/SavingsAccount.java
@@ -0,0 +1,31 @@
+package lt.techin;
+
+public class SavingsAccount {
+
+    public String owner;
+    public double balanceDollar;
+    public double balanceEuro;
+
+    public SavingsAccount(String owner, double balanceDollar){
+        // Complete the constructor
+        this.owner = owner;
+        this.balanceDollar = balanceDollar;
+        this.balanceEuro = balanceDollar * 0.85;
+
+    }
+
+    public void addMoney(int balanceDollar){
+        // Complete this method
+        System.out.println("Adding " + balanceDollar + " dollars to the account." );
+        this.balanceDollar += balanceDollar;
+        System.out.println("The new balance is " + this.balanceDollar + " dollars." );
+    }
+
+//    public double addMoney(double balanceDollar){
+//        // Complete this method
+//        System.out.println("Adding " + balanceDollar + " dollars to the account." );
+//        double currentBalance = this.balanceDollar += balanceDollar;
+//        System.out.println("The new balance is " + currentBalance + " dollars." );
+//        return balanceDollar;
+//    }
+}
