d8c250dabf81407f07511c9cb38559123f194b80
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2025-04-26 22:29:34 +0300

    New SQL task

---
+++ 2025_Mysql/Hello.js
@@ -0,0 +1,7 @@
+const x = "my first app!";
+
+console.log(x);
+
+const y = "my second app!.";
+
+console.log(y);
---
+++ 2025_Mysql/db.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql2");
+
+// Create a connection
+const connection = mysql.createConnection({
+  host: "localhost", // your host, usually localhost
+  user: "root", // your MySQL username
+  password: "rbjavacoding@", // your MySQL password
+  database: "book_shop", // your database name
+});
+
+// Connect
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting: " + err.stack);
+    return;
+  }
+  console.log("Connected as id " + connection.threadId);
+});
+
+// Don't forget to close the connection when done
+connection.end();
